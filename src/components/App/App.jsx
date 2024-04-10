f0397163034d25f819d907f99e2a91d82936eb5a
Nuclear303 <oliwier.kotlowski1@gmail.com>
2024-04-10 21:58:39 +0200

    implementacja podstawowego interfejsu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react"
 import db from "../../db.json"
+import Converter from "../Converter/Converter";
 
 export default function App(){
     const [index, setIndex] = useState(0);
 
 
     return(
-        <>
+        <div id="container">
+            <Converter/>
             <input type="number" name="" id="" value={db[Object.keys(db)[index]].price}/>
             <button onClick={_=>{setIndex(i => i+1)}}>click</button>
-        </>
+        </div>
     )
 }
