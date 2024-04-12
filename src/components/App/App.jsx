c31e48b5192a230124fc5d357610c807f0d6bea4
Nuclear303 <oliwier.kotlowski1@gmail.com>
2024-04-12 22:07:32 +0200

    remove debug code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react"
 import db from "../../db.json"
 import Converter from "../Converter/Converter";
+import "./App.css"
+import "./reset.css"
+
 
 export default function App(){
-    const [index, setIndex] = useState(0);
 
 
     return(
-        <div id="container">
+        <div className="container">
             <Converter/>
-            <input type="number" name="" id="" value={db[Object.keys(db)[index]].price}/>
-            <button onClick={_=>{setIndex(i => i+1)}}>click</button>
+            {/* <input type="number" name="" id="" value={db[Object.keys(db)[index]].price}/>
+            <button onClick={_=>{setIndex(i => i+1)}}>click</button> */}
         </div>
     )
 }
