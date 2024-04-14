1ea21bca9bf7d5667c903701240b035921704257
Nuclear303 <oliwier.kotlowski1@gmail.com>
2024-04-14 21:38:15 +0200

    Added table with buy/sell prices

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,35 @@
+import db from "../../db.json"
+import "./Table.css"
+
+export default function Table(){
+    return(
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Waluta</th>
+                        <th>Kupno</th>
+                        <th>Sprzedaż</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Object.keys(db).map((obj, index)=>{
+                            if(obj!=="pln" && index < 11){
+                                return (
+                                    <tr key={db[obj].symbol.split(" ")[0]}>
+                                        <td>{db[obj].symbol.split(" ")[0]}</td>
+                                        <td>{Number(db["pln"].price/db[obj].price*1.03).toFixed(4)}</td>
+                                        <td>{Number(db["pln"].price/db[obj].price*0.98).toFixed(4)}</td>
+                                    </tr>
+                                )
+                            }
+                            return"";
+                            
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
+    )
+}
